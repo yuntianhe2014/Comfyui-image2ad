911f0715f148440c541c03a63b40ca395b23a2c6
yuntianhe2014 <143938582+yuntianhe2014@users.noreply.github.com>
2024-07-04 15:23:18 +0800

    Update config.py
    更新

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-appid = "505dbe71"    #填写控制台中获取的 APPID 信息
-api_secret = "MDA2Yzg4NWUwYzM4NTI2MzYxZmU4M2Mz"   #填写控制台中获取的 APISecret 信息
-api_key ="c289125e12a284a070e83f3e8f767a50"    #填写控制台中获取的 APIKey 信息
+appid = ""    #填写控制台中获取的 APPID 信息
+api_secret = ""   #填写控制台中获取的 APISecret 信息
+api_key =""    #填写控制台中获取的 APIKey 信息
